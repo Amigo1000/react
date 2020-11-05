4f691b6dbfaf697520f98addd7dfdf95db0ddc67
Amigo1000 <sitodemmers@gmail.com>
2020-11-05 12:32:09 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,52 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDom from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+//Css
+import "./index.css";
+
+function BookList() {
+  return (
+    <div className="title">
+      <Title />
+      <section className="booklist">
+        <Book job="Developer" age={54} />
+        <Book job="Cook" age={34} />
+        <Book />
+        <Book />
+        <Book />
+        <Book />
+      </section>
+    </div>
+  );
+}
+
+const Title = () => {
+  return <h1 className="pageTitle">Book List</h1>;
+};
+
+const Book = (props) => {
+  const { job, age } = props;
+  return (
+    <article className="book">
+      <Image />
+      <BookTitle />
+      <Author />
+      <p>{job}</p>
+      <p>{age}</p>
+    </article>
+  );
+};
+
+const Image = () => (
+  <img src="https://image.shutterstock.com/image-photo/image-150nw-737761441.jpg" />
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const BookTitle = () => {
+  return <p className="postTitle">The night of Halloween</p>;
+};
+
+const Author = () => {
+  return <p>Sito Demmers</p>;
+};
+
+ReactDom.render(<BookList />, document.getElementById("root"));
